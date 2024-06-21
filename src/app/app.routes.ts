1001a7315b5b22572a82cee742142abd0e13f676
Bruno Joao <1353905+vinagreti@users.noreply.github.com>
2024-06-21 13:57:15 -0300

    basic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { ProjectsPageComponent } from "./pages/public/project/projects-page/projects-page.component";
+import { HomePageComponent } from "./pages/public/home-page/home-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: HomePageComponent,
+  },
+  {
+    path: "projects",
+    component: ProjectsPageComponent,
+  },
+];
