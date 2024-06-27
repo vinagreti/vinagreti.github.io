48c8d839860079713be42833ba1a8df4693240cc
Bruno Joao <1353905+vinagreti@users.noreply.github.com>
2024-06-27 16:26:48 -0300

    cvc

--- src/app/pages/public/project/projects-page/projects.ts
+++ src/app/pages/public/project/projects-page/projects.ts
@@ -46,7 +46,7 @@ As the social media environment changes constantly I have to help the company to
   },
   {
     name: "Avail Car Sharing",
-    from: "2022-06-01",
+    from: "2022-06-16",
     to: "2023-06-27",
     tools: ["React", "Redux", "Javascript", "HTML", "CSS", "Node", "Figma"],
     position: "Senior UI Engineer",
@@ -60,6 +60,68 @@ Responsibilities:
 My responsabilities includes creating and maintaining well written and documented front and back-end applications.
 Working with Javascript, NodeJs, React, Jira, Github, Jenkings, Kafka and other tools and services, we support the business with fast and secure response to the business needs trying to be ready for any request the company does.The company has multiple systems that support a Car Sharing and Rent business. Responsibilities: I'm responsible for creating and maintaining well written and documented front and back-end applications. Working with Javascript, NodeJs, React, Jira, Github, Jenkings, Kafka and other tools and services, we support the business with fast and secure response to the business needs trying to be ready for any request the company does.
 Web Development, React.js and +3 skills
+    `,
+  },
+  {
+    name: "Reef.ai",
+    from: "2021-06-01",
+    to: "2022-06-15",
+    tools: [
+      "Angular",
+      "Graphql",
+      "Javascript",
+      "HTML",
+      "CSS",
+      "Node",
+      "Figma",
+      "Jenkins",
+      "Bff",
+      "Microservices",
+    ],
+    position: "Senior UI Engineer",
+    github: null,
+    thumb: "/images/thumb-reef.webp",
+    desc:
+      `Reef uses AI to improve customer success. My main role was to develop an app to help companies to measure and plan the relationship with the customers.
+
+Responsibilities:
+
+I was responsible for creating and maintaining well written and documented front-end applications.
+Working with Javascript, Angular, Firebase, Bitbucket, Github Actions, Git, AWS, Google Cloud and other tools and services, we support the business with fast and secure response to the business needs trying to be ready for any request the company does.
+    `,
+  },
+  {
+    name: "Reef.ai",
+    from: "2020-12-01",
+    to: "2022-06-26",
+    tools: [
+      "React",
+      "NextJS",
+      "Javascript",
+      "HTML",
+      "CSS",
+      "Node",
+      "Figma",
+      "Jenkins",
+      "Microservices",
+    ],
+    position: "Software Engeneering III",
+    github: null,
+    thumb: "/images/thumb-cvc.png",
+    desc:
+      `Search for rooms, search for flights, search for traveling programs, request transfer, easing burocracy or whatever aspect of your travel you can do with CVC Corp products.
+
+We work to ensure that everyone from everywhere can plan, buy, enjoy and come back from their adeventure.
+
+Projects:
+
+Focused on front-end, working with the Hotels team, I'm responsible by making the hotels search as easy, secure and fast as possible.
+
+Using micro frontends with react and next.js, using Jira to manage the work flow, Jenkins to control deploy processes and Kubernets/Docker to run and delivery the applications, we have a very strong technical set.
+
+Responsibilities:
+
+As a Developer III I'm responsible by ensure we can solve any problem and create any feature the business needs.
     `,
   },
 ];
