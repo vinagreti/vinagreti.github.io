085ffcd85d74f233ad4e9dafe89addab94389501
Bruno Joao <1353905+vinagreti@users.noreply.github.com>
2024-06-27 15:33:49 -0300

    avail

--- src/app/pages/public/project/projects-page/projects-page.component.ts
+++ src/app/pages/public/project/projects-page/projects-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { projects } from "./projects";
 import { DatePipe, NgFor, NgIf } from "@angular/common";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-projects-page",
   standalone: true,
-  imports: [NgFor, DatePipe, NgIf],
+  imports: [NgFor, DatePipe, NgIf, RouterLink],
   templateUrl: "./projects-page.component.html",
   styleUrl: "./projects-page.component.scss",
 })
--- src/app/pages/public/project/projects-page/projects.ts
+++ src/app/pages/public/project/projects-page/projects.ts
@@ -27,9 +27,9 @@ The Idea is to showcase some libs, some apps and sideprojects I have done.
   },
   {
     name: "Slate",
-    from: null,
-    to: null,
-    tools: ["Angular", "Redux", "Javascript", "HTML", "CSS", "Node"],
+    from: "2023-09-19",
+    to: "2024-05-29",
+    tools: ["Angular", "Redux", "Javascript", "HTML", "CSS", "Node", "Figma"],
     position: null,
     github: null,
     thumb: "/images/thumb-slate.png",
@@ -42,6 +42,24 @@ Web client used to manage assets and create contents.
 Responsibilities:
 Working on the web client my responsabilities includes maintaining the application and adding new features.
 As the social media environment changes constantly I have to help the company to evolve in the speed needed and with the quality it needs.
+    `,
+  },
+  {
+    name: "Avail Car Sharing",
+    from: "2022-06-01",
+    to: "2023-06-27",
+    tools: ["React", "Redux", "Javascript", "HTML", "CSS", "Node", "Figma"],
+    position: null,
+    github: null,
+    thumb: "/images/thumb-avail.png",
+    desc:
+      `The company has multiple systems that support a Car Sharing and Rent business.
+
+Responsibilities:
+
+My responsabilities includes creating and maintaining well written and documented front and back-end applications.
+Working with Javascript, NodeJs, React, Jira, Github, Jenkings, Kafka and other tools and services, we support the business with fast and secure response to the business needs trying to be ready for any request the company does.The company has multiple systems that support a Car Sharing and Rent business. Responsibilities: I'm responsible for creating and maintaining well written and documented front and back-end applications. Working with Javascript, NodeJs, React, Jira, Github, Jenkings, Kafka and other tools and services, we support the business with fast and secure response to the business needs trying to be ready for any request the company does.
+Web Development, React.js and +3 skills
     `,
   },
 ];
