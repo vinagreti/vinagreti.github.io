7ae50e1c0674d35e8f39da1fd27e775089f2faf5
Bruno Joao <1353905+vinagreti@users.noreply.github.com>
2024-09-03 01:08:00 -0300

    add note to group

--- src/app/pages/private/notes/note-add-page/note-add-page.component.ts
+++ src/app/pages/private/notes/note-add-page/note-add-page.component.ts
@@ -1,14 +1,15 @@
-import { AsyncPipe } from "@angular/common";
+import { AsyncPipe, NgFor } from "@angular/common";
 import { Component, inject } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
 import { NoteService } from "@services/note/note.service";
+import { INoteGroup } from "@services/note/note.types";
 import { BehaviorSubject } from "rxjs";
 
 @Component({
   selector: "app-note-add-page",
   standalone: true,
-  imports: [AsyncPipe, FormsModule],
+  imports: [AsyncPipe, FormsModule, NgFor],
   templateUrl: "./note-add-page.component.html",
   styleUrl: "./note-add-page.component.scss",
 })
@@ -23,18 +24,30 @@ export class NoteAddPageComponent {
 
   content: string = "";
 
+  noteGroup: string = this.route.snapshot.params["noteGroupId"];
+
   waitingCreation$ = new BehaviorSubject(false);
+
+  noteGroups$ = new BehaviorSubject<INoteGroup[]>([]);
+
+  constructor() {
+    this.loadNoteGroups();
+  }
+
+  private async loadNoteGroups() {
+    const noteGroups = await this.noteService.list();
+    this.noteGroups$.next(noteGroups);
+  }
 
   async addNote() {
     this.waitingCreation$.next(true);
-    const noteGroupId = this.route.snapshot.params["noteGroupId"];
-    const { error, note } = await this.noteService.add(noteGroupId, {
+    const { error, note } = await this.noteService.add(this.noteGroup, {
       title: this.title,
       content: this.content,
     });
     if (note) {
-      await this.router.navigate(["/notes", noteGroupId]);
-      this.router.navigate(["/notes", noteGroupId, note.id]);
+      await this.router.navigate(["/notes", this.noteGroup]);
+      this.router.navigate(["/notes", this.noteGroup, note.id]);
     } else {
       alert("Error adding note group!!!");
       console.log("Error adding note group", error.code, error.message);
