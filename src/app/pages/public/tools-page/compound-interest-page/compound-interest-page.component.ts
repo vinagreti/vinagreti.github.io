17d01a7522e9a9bafed547ff1d57574d1b0bdfdb
Bruno Joao <1353905+vinagreti@users.noreply.github.com>
2024-09-24 20:07:47 -0300

    interest compound

--- src/app/pages/public/tools-page/compound-interest-page/compound-interest-page.component.ts
+++ src/app/pages/public/tools-page/compound-interest-page/compound-interest-page.component.ts
@@ -1,18 +1,27 @@
-import { AsyncPipe } from "@angular/common";
+import { AsyncPipe, CurrencyPipe } from "@angular/common";
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { ReplaySubject } from "rxjs";
+import { combineLatest, map, ReplaySubject } from "rxjs";
 
 @Component({
   selector: "app-compound-interest-page",
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [FormsModule, AsyncPipe],
+  imports: [FormsModule, AsyncPipe, CurrencyPipe],
   templateUrl: "./compound-interest-page.component.html",
   styleUrl: "./compound-interest-page.component.scss",
 })
 export class CompoundInterestPageComponent {
-  total$ = new ReplaySubject<number>();
+  totalWithInterest$ = new ReplaySubject<number>();
+
+  totalWithoutInterest$ = new ReplaySubject<number>();
+
+  gain$ = combineLatest([this.totalWithInterest$, this.totalWithoutInterest$])
+    .pipe(
+      map(([totalWithInterest, totalWithoutInterest]) =>
+        totalWithInterest - totalWithoutInterest
+      ),
+    );
 
   data = {
     times: 0,
@@ -22,6 +31,15 @@ export class CompoundInterestPageComponent {
   };
 
   calc() {
-    this.total$.next(100);
+    const { initialValue, interest, monthlyDeposit, times } = this.data;
+    let totalWithInterest = initialValue;
+    let totalWithoutInterest = initialValue;
+    for (let i = 0; i < times; i++) {
+      const totalWithMothly = (totalWithInterest * interest) + monthlyDeposit;
+      totalWithInterest += totalWithMothly;
+      totalWithoutInterest += monthlyDeposit;
+    }
+    this.totalWithInterest$.next(totalWithInterest);
+    this.totalWithoutInterest$.next(totalWithoutInterest);
   }
 }
