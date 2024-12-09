a3ee70f57203d1d38d04a52c00c9b381f822f23d
Bruno Joao <1353905+vinagreti@users.noreply.github.com>
2024-12-08 23:35:04 -0300

    pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,5 +11,8 @@ export const appConfig: ApplicationConfig = {
           }), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
           })]
 };
